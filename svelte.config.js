b3aedfaca5bb982055f8129957d0a6686dac5203
Richard Fu <richardfxr.personal@gmail.com>
2022-02-11 22:51:56 -0800

    🔧CNFG: SCSS prepends all files in src/styles as *

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,10 @@ const config = {
 	},
 
 	preprocess: preprocess({
-		// ...svelte-preprocess options
+		scss: {
+			// prepend all src/styles SCSS files to each component
+			prependData: `@use 'src/styles' as *;`
+		}
 	}),
 };
 
