c7c71496094ab0bb5af8a78e1d5e319f4c641ce8
Richard Fu <richardfxr.personal@gmail.com>
2022-02-18 23:20:11 -0500

    🟠SVLT: added a11yMenu to index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import A11yMenu from '$lib/a11yMenu.svelte';
+
+    const a11yMenuOptions = [
+        {text: "Skip to content.", href: "#main"},
+        {text: "Navigation.", href: "#"},
+        {text: "Settings.", href: "#"},
+        {text: "Accessibility Help.", href: "#"}
+    ]
+</script>
+
+
+
+<A11yMenu 
+    options={a11yMenuOptions}
+    --bgColor="var(--clr-100)"
+    --bgColorHover="var(--clr-50)"/>
+<h1>sity.earth v0.2.0 pre-alpha</h1>
+<p>Page still in the works. See current published build at <a href="https://sity.earth/" target="_blank">sity.earth</a>.</p>
