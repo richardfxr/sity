687dbebe225074e6feb5c4e0bf3315e093c68aaf
Richard Fu <richardfxr.personal@gmail.com>
2022-03-07 15:53:29 -0500

    ✨NEW: __layout.svelte which calls initFirebase()

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+    // following code runs on both server and client
+
+    import { initFirebase } from '../firebase/init';
+
+    console.log("should be able to see this");
+
+    export async function load() {
+        // initialize Firebase and Firestore
+        let db = await initFirebase();
+
+        return {
+            props: {
+            }
+        }
+    }
+</script>
+
+<script>
+
+</script>
+
+<slot />
