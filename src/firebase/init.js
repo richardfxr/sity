794792b1d64f56dfb82a43f49db0707ad8247486
Richard Fu <richardfxr.personal@gmail.com>
2022-03-07 15:54:11 -0500

    ✨NEW: init.js - inits Firebase & Firestore

---
+++ src/firebase/init.js
@@ -0,0 +1,50 @@
+import { browser } from '$app/env';
+
+export async function initFirebase() {
+    if (browser) {
+        // following code only runs in a browser (browser === true)
+
+        // browser-only imports (Firebase 9 SDK)
+        const { initializeApp, getApps, getApp } = await import("firebase/app");
+        const { getFirestore } = await import ("firebase/firestore");
+
+        if (getApps().length === 0) {
+            // import firebaseConfig
+            const { firebaseConfig } = await import("./config");
+
+            // init firebase if one doesn't already exist
+            initializeApp(firebaseConfig);
+        } else {
+            // get firebaseApp if it already exists
+            getApp();
+        }
+
+        // initialize Firestore
+        const db = getFirestore();
+        return db;
+
+    } else {
+        // following code only runs on the server
+
+        // server-only imports (Firebase Admin SDK)
+        const { initializeApp, applicationDefault, cert, getApps, getApp } = await import('firebase-admin/app');
+        const { getFirestore, Timestamp, FieldValue } = await import ('firebase-admin/firestore');
+
+        if (getApps().length === 0) {
+            // import service account private key
+            const serviceAccount = await import ('../.secure/firebaseAdminKey.json');
+
+            // init firebase if one doesn't already exist
+            initializeApp({
+                credential: cert(serviceAccount)
+            });
+        } else {
+            // get firebaseApp if it already exists
+            getApp();
+        }
+
+        // initialize Firestore
+        const db = getFirestore();
+        return db;
+    }
+}
