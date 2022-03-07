485f04f4828e1d927f80d38551573b9af4786d98
Richard Fu <richardfxr.personal@gmail.com>
2022-03-07 13:59:41 -0500

    🔧CNFG: added Firebase config object

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyD4REs-oq2z8Y6WMioANGco7qKx0cFStlU",
+    authDomain: "sity-earth.firebaseapp.com",
+    projectId: "sity-earth",
+    storageBucket: "sity-earth.appspot.com",
+    messagingSenderId: "127777405556",
+    appId: "1:127777405556:web:509d3e68660e5629a1ef21",
+    measurementId: "G-RXZRT6SQMH"
+};
