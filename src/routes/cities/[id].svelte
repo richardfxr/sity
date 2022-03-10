24da1aa8a2f562754776876177fa908ba0ed04bc
Richard Fu <richardfxr.personal@gmail.com>
2022-03-10 12:49:37 -0500

    ✨NEW: created dynamic cities/[id].svelte

---
+++ src/routes/cities/[id].svelte
@@ -0,0 +1,21 @@
+<script context="module">
+    export async function load({ params, fetch, session, stuff }) {
+        const id = params.id;
+
+        return {
+            props: {
+                id
+            }
+        };
+    }
+</script>
+
+<script>
+    export let id = "unknown";
+</script>
+
+<h1>{id}</h1>
+
+<style lang="scss">
+
+</style>
