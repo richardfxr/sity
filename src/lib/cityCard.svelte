1ca7e3a6c92f7df49d2b9d5e40442732faec7029
Richard Fu <richardfxr.personal@gmail.com>
2022-03-10 12:03:02 -0500

    ✨NEW: created cityCard component

---
+++ src/lib/cityCard.svelte
@@ -0,0 +1,20 @@
+<script>
+    // props
+    export let id = "#";
+    export let name = "N/A";
+    export let state = "N/A";
+</script>
+
+<a class="cityCard" href="{id}">
+    <h2>{name}</h2>
+    <span class="state">{state}</span>
+</a>
+
+<style lang="scss">
+    .cityCard {
+        display: block;
+        padding: 1.8rem;
+        background-color: var(--clr-100);
+        border-radius: 1rem;
+    }
+</style>
