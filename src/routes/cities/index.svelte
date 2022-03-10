74ab09c02328cbb5394e756f5f569244d90c1dd6
Richard Fu <richardfxr.personal@gmail.com>
2022-03-10 12:50:42 -0500

    ✨NEW: /cities redirects to /#search

---
+++ src/routes/cities/index.svelte
@@ -0,0 +1,9 @@
+<script context="module">
+    export async function load({ params, fetch, session, stuff }) {
+        return {
+            // redirect "/cities" to search section on home page
+            status: 302,
+            redirect: "/#search"
+        }
+    }
+</script>
