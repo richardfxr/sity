cf5eb698bc8d55493d6199e67ee2646ebf9d0481
Richard Fu <richardfxr.personal@gmail.com>
2022-03-19 13:03:37 -0400

    🟠SVLT: added cityOptCard to city/[id]

--- src/routes/city/[id].svelte
+++ src/routes/city/[id].svelte
@@ -49,8 +49,8 @@
     // imports
     import A11yMenu from '$lib/a11yMenu.svelte';
     import SvgIcon from '$lib/svgIcon.svelte';
+    import CityOptCard from '$lib/cityOptCard.svelte';
     import Button from '$lib/button.svelte';
-    import SvgShape from "$lib/svgShape.svelte";
     import {copyURL} from '$lib/clipboard.js';
 
     // button options for A11yMenu
@@ -99,28 +99,14 @@
 
             <ul class="contentGrid cityOpt">
                 <li class="guidelines">
-                    <a href="#guidelines" target="_blank" class="cityOptCard">
-                        <!-- SVG shapes -->
-                        <SvgShape shape="Plastic" --bgColor="var(--clr-plastic-150)" />
-                        <SvgShape shape="Cardboard" --bgColor="var(--clr-cardboard-150)" />
-                        <SvgShape shape="Metal" --bgColor="var(--clr-metal-150)" />
-                        <SvgShape shape="Glass" --bgColor="var(--clr-glass-150)" />
-                        <SvgShape shape="Paper" --bgColor="var(--clr-paper-150)" />
-                        
-                        <h2>City guidelines</h2>
-                        <p class="text--xs">{guideline.categories.length} categories</p>
-                    </a>
+                    <CityOptCard id="guidelines" guidelineLength={guideline.categories.length} />
                 </li>
                 <li class="share">
                     <Button type="button" style="lg--icon" icon="share" text="Share this page" on:click={copyURL} --inlineSize="auto" --textColor="var(--clr-900)" --textColorHover="var(--clr-0)" --bgColor="var(--clr-100)" --bgColorHover="var(--clr-700)" --bgColorTransition="var(--clr-250)"/>
                     <Button type="button" style="lg--icon" icon="share" text="Share" on:click={copyURL} --inlineSize="auto" --textColor="var(--clr-900)" --textColorHover="var(--clr-0)" --bgColor="var(--clr-100)" --bgColorHover="var(--clr-700)" --bgColorTransition="var(--clr-250)"/>
                 </li>
                 <li  class="facility">
-                    <a href={guideline.link} target="_blank" class="cityOptCard">
-                        <SvgIcon icon="arrowTopRight" --mainClr="var(--clr-900)" />
-                        <p class="text--xs">Recycling processed by</p>
-                        <h2>{guideline.name}</h2>
-                    </a>
+                    <CityOptCard id="facility" href={guideline.link} facilityName={guideline.name} />
                 </li>
             </ul>
         </div>
@@ -139,77 +125,8 @@
             "share facility" 5rem
             / 1fr 1fr;
 
-        &Card {
-            display: flex;
-            flex-flow: column nowrap;
-            gap: var(--pad-xxs);
-            padding: var(--pad-main);
-            background-color: var(--clr-100);
-
-            /* round corners and prevent overflow */
-            border-radius: var(--border-radius);
-            overflow: hidden;
-
-            /* relative position for .svgShape */
-            position: relative;                
-
-            h2 {
-                color: var(--clr-900);
-            }
-
-            p {
-                color: var(--clr-900);
-                opacity: 0.85;
-            }
-        }
-
         .guidelines {
             grid-area: guidelines;
-
-            a {
-                height: 100%;
-                /* push text to bottom of card */
-                justify-content: flex-end;
-            }
-
-            // SvgShape positioning
-            :global(.svgShape) {
-                position: absolute;
-            }
-
-            :global(.plastic) {
-                width: 45%;
-                transform: rotate(50deg);
-                top: 13%;
-                left: 67%;
-            }
-
-            :global(.cardboard) {
-                width: 46%;
-                top: -13%;
-                left: 45%;
-            }
-
-            :global(.metal) {
-                width: 40%;
-                transform: rotate(110deg);
-                top: 13%;
-                left: -14%;
-            }
-
-            :global(.glass) {
-                width: 45%;
-                transform: rotate(50deg);
-                top: -15%;
-                left: 11%;
-            }
-
-            :global(.paper) {
-                width: 49%;
-                transform: rotate(-20deg);
-                top: -4%;
-                left: 18%;
-            }
         }
 
         .share {
@@ -228,16 +145,6 @@
             /* align facility to the bottom */
             display: flex;
             justify-content: flex-end;
-
-            a {
-                margin-top: auto;
-            }
-
-            :global(svg) {
-                width: 2rem;
-                margin-left: auto;
-                padding-bottom: 2rem;
-            }
         }
     }
 
