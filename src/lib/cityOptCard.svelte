60fcc2d4b49a3ac85fef6720e090281486520472
Richard Fu <richardfxr.personal@gmail.com>
2022-03-19 13:32:49 -0400

    💄CSS: added reactive styling to cityOptCard

--- src/lib/cityOptCard.svelte
+++ src/lib/cityOptCard.svelte
@@ -34,6 +34,10 @@
 
 <style lang="scss">
     .cityOptCard {
+        /* variables */
+        --scaleHover: 1.03;
+        --textTransY: -1rem;
+
         display: flex;
         flex-flow: column nowrap;
         gap: var(--pad-xxs);
@@ -45,10 +49,52 @@
         overflow: hidden;
 
         /* relative position for .svgShape */
-        position: relative;                
+        position: relative;
+
+        transform: scale(1);
+        transition: transform var(--transition-300) var(--transition-smoothEase),
+                    background-color var(--transition-200) ease-in-out;
+        
+        &::before {
+            /* ::before pseudo class that appears as a circle when hovered/focused */
+            content: "";
+            border-radius: var(--border-radius);
+            background-color: var(--clr-150);
+            /* move ::before under main text */
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            z-index: 0;
+
+            transform: translateY(50%) scale(0);
+            transition: transform var(--transition-300) var(--transition-smoothEase),
+                        background-color var(--transition-200) ease-in-out;
+        }
+
+        &:hover, &:focus-visible {
+            transform: scale(var(--scaleHover));
+
+            &::before {
+                transform: translateY(0%) scale(1);  
+            }
+
+            h2, p {
+                transform: translateY(var(--textTransY));
+            }
+        }
+
+        h2, p {
+            transform: translateY(0);
+            transition: transform var(--transition-300) var(--transition-smoothEase);
+        }
 
         h2 {
             color: var(--clr-900);
+            /* move h2 above ::before */
+            position: relative;
+            z-index: 1;
         }
 
         p {
@@ -63,9 +109,32 @@
             /* push text to bottom of card */
             justify-content: flex-end;
 
+            &:hover, &:focus-visible {
+                :global(.plastic) {
+                    transform: translate(20%, -10%) rotate(60deg);
+                }
+
+                :global(.cardboard) {
+                    transform: translate(5%, -20%) rotate(7deg);
+                }
+
+                :global(.metal) {
+                    transform: translate(-15%, -15%) rotate(97deg);
+                }
+
+                :global(.glass) {
+                    transform: translate(-5%, -23%) rotate(40deg);
+                }
+
+                :global(.paper) {
+                    transform: translate(2%, -25%) rotate(-6deg);
+                }
+            }
+
             // SvgShape positioning
             :global(.svgShape) {
                 position: absolute;
+                transition: transform var(--transition-300) var(--transition-smoothEase);
             }
 
             :global(.plastic) {
@@ -108,10 +177,77 @@
         &#facility {
             margin-top: auto;
 
+            &:hover, &:focus-visible {
+                :global(svg) {
+                    transform: translate(10%, -10%);
+                }
+            }
+
             :global(svg) {
                 width: 2rem;
                 margin-left: auto;
                 padding-bottom: 2rem;
+                /* move svgIcon above ::before */
+                position: relative;
+                z-index: 1;
+
+                transform: translate(0);
+                transition: transform var(--transition-300) var(--transition-smoothEase);
+            }
+        }
+    }
+
+
+    /* reduced motion */
+    @media (prefers-reduced-motion) {
+        :global([data-motion="auto"]) {
+            .cityOptCard {
+                /* variables - prevent scailing and translate */
+                --scaleHover: 1;
+                --textTransY: 0;
+
+                &::before {
+                    /* hide ::before element */
+                    display: none;
+                }
+
+                &:hover, &:focus-visible {
+                    background-color: var(--clr-150);
+                }
+
+                &#guidelines {
+                    &:hover, &:focus-visible {
+                        // remove all svgShape movements
+                        :global(.plastic) {
+                            transform: rotate(50deg);
+                        }
+
+                        :global(.cardboard) {
+                            transform: rotate(0deg);
+                        }
+
+                        :global(.metal) {
+                            transform: rotate(110deg);
+                        }
+
+                        :global(.glass) {
+                            transform: rotate(50deg);
+                        }
+
+                        :global(.paper) {
+                            transform: rotate(-20deg);
+                        }
+                    }
+                }
+
+                &#facility {
+                    &:hover, &:focus-visible {
+                        // remove svgIocn translate
+                        :global(svg) {
+                            transform: translate(0);
+                        }
+                    }
+                }
             }
         }
     }
@@ -120,6 +256,9 @@
     /* === BREAKPOINTS ======================== */
     @media only screen and (max-width: $breakpoint-mobile) {
         .cityOptCard {
+            /* variables */
+            --textTransY: -3vw;
+
             &#facility {
                 :global(svg) {
                     width: 6vw;
