6884df2c24ad5a67d74d796a4f8f801569314569
Richard Fu <richardfxr.personal@gmail.com>
2022-03-30 00:25:09 -0400

    🟠SVLT: removed a11yMenu and added page variables

--- src/routes/city/[id].svelte
+++ src/routes/city/[id].svelte
@@ -47,11 +47,17 @@
     export let guideline;
 
     // imports
-    import A11yMenu from '$lib/a11yMenu.svelte';
+    import { curPage, hasCity, defaultCity } from '../../store/store.js';
     import SvgIcon from '$lib/svgIcon.svelte';
     import CityOptCard from '$lib/cityOptCard.svelte';
     import Button from '$lib/button.svelte';
     import {copyURL} from '$lib/clipboard.js';
+
+    // set curPage
+    curPage.set("city");
+    // set city
+    hasCity.set(true);
+    defaultCity.set(id);
 
     // button options for A11yMenu
     const a11yMenuOptions = [
@@ -63,15 +69,6 @@
 </script>
 
 
-
-<A11yMenu
-    options={a11yMenuOptions}
-    --textColorHover="var(--clr-0)"
-    --menuBgColor="var(--clr-100)"
-    --bgColor="var(--clr-150)"
-    --bgColorHover="var(--clr-700)"
-    --bgColorTransition="var(--clr-250)"/>
-
 <main id="main">
     <div class="twoCol maxWidth" id="intro">
         <div class="hero">
@@ -80,30 +77,19 @@
                 <a href="/">
                     <SvgIcon icon="logotype"/>
                 </a>
-                <!-- tablet/mobile user auth buttons -->
-                <div class="userAuth">
-                    <Button type="link" text="Log in" href="#" --inlineSize="auto" --textColorHover="var(--clr-0)" --bgColor="var(--clr-150)" --bgColorHover="var(--clr-700)" --bgColorTransition="var(--clr-250)"/>
-                    <Button type="link" text="Sign up" href="#" --inlineSize="auto" --textColorHover="var(--clr-0)" --bgColor="var(--clr-150)" --bgColorHover="var(--clr-700)" --bgColorTransition="var(--clr-250)"/>
-                </div>
             </div>
 
             <h1>Recycling in <br><span class="accent">{city.name}, {city.state}</span></h1>
         </div>
 
         <div class="content">
-            <div class="header">
-                <!-- desktop user auth buttons -->
-                <Button type="link" text="Log in" href="#" --inlineSize="auto" --textColorHover="var(--clr-0)" --bgColor="var(--clr-150)" --bgColorHover="var(--clr-700)" --bgColorTransition="var(--clr-250)"/>
-                <Button type="link" text="Sign up" href="#" --inlineSize="auto" --textColorHover="var(--clr-0)" --bgColor="var(--clr-150)" --bgColorHover="var(--clr-700)" --bgColorTransition="var(--clr-250)"/>
-            </div>
-
             <ul class="contentGrid cityOpt">
                 <li class="guidelines">
                     <CityOptCard id="guidelines" guidelineLength={guideline.categories.length} />
                 </li>
                 <li class="share">
-                    <Button type="button" style="lg--icon" icon="share" text="Share this page" on:click={copyURL} --inlineSize="auto" --textColor="var(--clr-900)" --textColorHover="var(--clr-0)" --bgColor="var(--clr-100)" --bgColorHover="var(--clr-700)" --bgColorTransition="var(--clr-250)"/>
-                    <Button type="button" style="lg--icon" icon="share" text="Share" on:click={copyURL} --inlineSize="auto" --textColor="var(--clr-900)" --textColorHover="var(--clr-0)" --bgColor="var(--clr-100)" --bgColorHover="var(--clr-700)" --bgColorTransition="var(--clr-250)"/>
+                    <Button type="button" style="lg--icon" icon="share" text="Share this page" on:click={copyURL} --inlineSize="auto" --textClr="var(--clr-900)" --textClrHover="var(--clr-0)" --bgClr="var(--clr-100)" --bgClrHover="var(--clr-700)" --bgClrTransition="var(--clr-250)"/>
+                    <Button type="button" style="lg--icon" icon="share" text="Share" on:click={copyURL} --inlineSize="auto" --textClr="var(--clr-900)" --textClrHover="var(--clr-0)" --bgClr="var(--clr-100)" --bgClrHover="var(--clr-700)" --bgClrTransition="var(--clr-250)"/>
                 </li>
                 <li  class="facility">
                     <CityOptCard id="facility" href={guideline.link} facilityName={guideline.name} />
@@ -111,10 +97,20 @@
             </ul>
         </div>
     </div>
+
+    <div class="placeholder"></div>
 </main>
 
 <style lang="scss">
     :global(html, body) {
+        /* page variables */
+        --pageClr-800: var(--clr-800);
+        --pageClr-700: var(--clr-700);
+        --pageClr-250: var(--clr-250);
+        --pageClr-150: var(--clr-150);
+        --pageClr-100: var(--clr-100);
+        --pageClr-0: var(--clr-0);
+
         background-color: var(--clr-50);
     }
 
