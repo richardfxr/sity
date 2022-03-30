69a87b5157a77c8fc2f1f8d51d6be631f019865d
Richard Fu <richardfxr.personal@gmail.com>
2022-03-30 17:20:16 -0400

    🟠SVLT: added persistent stores using localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,33 @@
+import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 
+// initialize stored variables (default values for server)
+let storedDefaultCity = null;
+let storedHasCity = false;
+
+if (browser) {
+    // get stored values from localStorage (null is acceptatble)
+    storedDefaultCity = localStorage.defaultCity;
+    storedHasCity = !!storedDefaultCity;
+}
+
+/* === STORES ============================= */
 export const curPage = writable(null);
-export const hasCity = writable(false);
-export const defaultCity = writable(null);
+export const hasCity = writable(storedHasCity);
+export const defaultCity = writable(storedDefaultCity);
+
+/* === UPDATES ============================ */
+defaultCity.subscribe((value) => {
+    // prevent code from running on server
+    if (!browser) return
+    
+    if (value) {
+        // set localStorage and change hasCity if value is truthy
+        localStorage.defaultCity = value;
+        hasCity.set(true);
+    } else {
+        // remove local storage and change hasCity if value is falsy
+        localStorage.removeItem("defaultCity");
+        hasCity.set(false);
+    }
+});
