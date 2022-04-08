f16b0294efbcbd7380b3c80582f7316bde4811ac
Richard Fu <richardfxr.personal@gmail.com>
2022-04-08 11:23:10 -0400

    💄CSS: added carCard styling

--- src/lib/catCard.svelte
+++ src/lib/catCard.svelte
@@ -31,23 +31,150 @@
     <SvgShape shape={category.id} ariaHidden={true} dataDepth={0.1 + category.depthOffset} bgClr={`var(--clr-${category.clr}-100)`} />
 
     <!-- SVG illustration -->
-    <Illustration illus={category.illus} clr={category.clr} ariaHidden={true} dataDepth={0.4} />
+    <Illustration illus={category.illus} clr1={category.illusClrs[0]} clr2={category.illusClrs[1]} clr3={category.illusClrs[2]} ariaHidden={true} dataDepth={0.4} />
 
-    <span data-depth="0.7" class="text--h1" style={`--clr: var(--clr-${category.clr}-1100`}>{category.name}</span>
+    <span data-depth="0.7" class="text--h1" style={`--clr: var(--clr-${category.clr}-1000`}>{category.name}</span>
 </a>
 
 
 <style lang="scss">
     .catCard {
         display: block;
+        position: relative;
+
+        // background shape
+        :global(.svgShape) {
+            // default positioning, same as in-line styling applied by parallax-js
+            position: relative;
+        }
+
+        // illustration
+        :global(.illus) {
+            // default positioning, same as in-line styling applied by parallax-js
+            position: absolute;
+        }
 
         span {
             display: block;
             color: var(--clr);
+            // default positioning, same as in-line styling applied by parallax-js
+            position: absolute;
         }
 
-        &.Carton {
+        // cartons category card
+        &.cartons {
+            // illustration
+            :global(.illus) {
+                width: 45%;
+                margin-top: 23%;
+                margin-left: 23%;
+            }
 
+            span {
+                margin-top: 70%;
+                margin-left: 20%;
+            }
+        }
+
+        // cardboard category card
+        &.cardboard {
+            // background shape
+            :global(.svgShape) {
+                width: 92%;
+                margin-top: 8%;
+                margin-left: 4%;
+            }
+
+            // illustration
+            :global(.illus) {
+                width: 77%;
+                margin-top: 30%;
+                margin-left: 11%;
+            }
+
+            span {
+                margin-top: 73%;
+                margin-left: 8%;
+            }
+        }
+
+        // paper category card
+        &.paper {
+            // background shape
+            :global(.svgShape) {
+                width: 96%;
+                margin-left: 2%;
+            }
+
+            // illustration
+            :global(.illus) {
+                width: 68%;
+                margin-top: 35%;
+                margin-left: 20%;
+            }
+
+            span {
+                margin-top: 70%;
+                margin-left: 30%;
+            }
+        }
+
+        // metal category card
+        &.metal {
+            // background shape
+            :global(.svgShape) {
+                width: 90%;
+                margin-top: 5%;
+                margin-left: 5%;
+            }
+
+            // illustration
+            :global(.illus) {
+                width: 38%;
+                margin-top: 30%;
+                margin-left: 30%;
+            }
+
+            span {
+                margin-top: 70%;
+                margin-left: 28%;
+            }
+        }
+
+        // glass category card
+        &.glass {
+            // background shape
+            :global(.svgShape) {
+                width: 90%;
+                margin-left: 5%;
+            }
+
+            // illustration
+            :global(.illus) {
+                width: 38%;
+                margin-top: 30%;
+                margin-left: 30%;
+            }
+
+            span {
+                margin-top: 70%;
+                margin-left: 35%;
+            }
+        }
+
+        // plastic category card
+        &.plastic {
+            // illustration
+            :global(.illus) {
+                width: 38%;
+                margin-top: 30%;
+                margin-left: 30%;
+            }
+
+            span {
+                margin-top: 70%;
+                margin-left: 20%;
+            }
         }
     }
     
