792157bcb5c0f3f513d3bc7e04a7b10f97571da8
Richard Fu <richardfxr.personal@gmail.com>
2022-04-15 15:43:50 -0400

    🟠SVLT: added illus & updated #heroIllus animation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,6 +26,7 @@
     import { onMount } from 'svelte';
     import { onPageLoad } from '$lib/page.js';
     import { curPage } from '../store/store.js';
+    import Illustration from '$lib/illustration.svelte';
     import SvgIcon from '$lib/svgIcon.svelte';
     import Button from '$lib/button.svelte';
     import SiteOptCard from '$lib/siteOptCard.svelte';
@@ -52,7 +53,8 @@
     /* === REACTIVE DECLARATIONS ============== */
     let scrollLimit = 0.8;
     $: scrollYRatio = scrollY / windowHeight;
-    $: scrollYScale = scrollYRatio / scrollLimit < 1 ? 1 - (scrollYRatio / scrollLimit)  : 0;
+    // scroll variable used for .heroIllus
+    $: scrollYInv = scrollYRatio / scrollLimit < 1 ? 1 - (scrollYRatio / scrollLimit)  : 0;
 </script>
 
 
@@ -60,11 +62,20 @@
 
 
 <div id="heroIllus">
-    <div class="container normalWidth">
-        <div class="main" style="transform: scale({1 + scrollYScale})">
-            
+    <div class="container">
+        <div class="normalWidth">
+            <div class="main" style="transform: scale({1 + scrollYInv}">
+                <div class="textCard" style="transform: translateY(calc({scrollYInv}* (-1 * var(--mainHeight))))">
+                    <span class="text--h1">Where does it all go?</span>
+                    <div class="handles">
+                        <!-- left bin handle -->
+                        <Illustration illus="binHandle-left" clr1="main" ariaHidden={true} />
+                        <!-- right bin handle -->
+                        <Illustration illus="binHandle-right" clr1="main" ariaHidden={true} />
+                    </div>
+                </div>
+            </div>
         </div>
-        <span class="text--h1">Where does it all go?</span>
     </div>
 </div>
 
@@ -143,39 +154,83 @@
         /* remove gap between #heroIllus and #intro */
         margin-bottom: calc(-1 * var(--pad-xxl));
 
-        /* prevent scale transform from overflowing and adding horizontal scroll bar */
-        contain: paint;
-
         .container {
             /* sticky positioning */
             position: sticky;
-            top: 18vh;
+            top: 0;
             right: 0;
             left: 0;
 
-            /* flexbox */
             display: flex;
-            flex-flow: column nowrap;
-            align-items: center;
+            align-items: flex-end;
             justify-content: center;
 
-            height: 64vh;
+            width: 100%;
+            height: 100vh;
+
+            /* prevent overflow from scaling */
+            overflow: hidden;
 
             .main {
-                width: 100%;
-                height: 100%;
+                /* variables */
+                --mainHeight: 68vh;
+                --borderWidth: 0;
                 
+                width: 100%;
+                height: var(--mainHeight);
+                
+                border: solid var(--clr-stroke) var(--borderWidth);
                 border-radius: var(--border-radius);
-                overflow: hidden;
 
-                background-color: var(--pageClr-100);
+                background-color: var(--pageClr-250);
+
+                .textCard {
+                    /* take up all of .main with space at bottom */
+                    width: calc(100% + 2 * var(--borderWidth));
+                    height: 95%;
+                    /* overlap borders */
+                    margin: 0 calc(-1 * var(--borderWidth));
+
+                    /* flexbox to center span */
+                    display: flex;
+                    flex-flow: column nowrap;
+                    align-items: center;
+                    justify-content: center;
+
+                    border: solid var(--clr-stroke) var(--borderWidth);
+                    border-radius: var(--border-radius);
+                    overflow: hidden;
+
+                    background-color: var(--pageClr-100);
+
+                    span {
+                        padding: 0 var(--pad-lg);
+                        text-align: center;
+                        font-size: min(5vw, calc(var(--max-width) * 0.05));
+                    }
+
+                    .handles {
+                        /* position handles to bottom of .textCard */
+                        position: absolute;
+                        right: 0;
+                        bottom: 0;
+                        left: 0;
+
+                        /* flexbox to push handle to sides */
+                        display: flex;
+                        flex-flow: row nowrap;
+                        justify-content: space-between;
+                        height: 14rem;
+                        padding: 0 2rem 2rem 2rem;
+
+                        :global(.illus) {
+                            height: 100%;
+                        }
+                    }
+                }
             }
 
-            span {
-                position: absolute;
-                padding: 0 var(--pad-sm);
-                text-align: center;
-            }
+            
         }
     }
 
@@ -234,6 +289,18 @@
 
     /* === BREAKPOINTS ======================== */
     @media only screen and (max-width: $breakpoint-smdesktop) {
+        #heroIllus {
+            .container {
+                .main {
+                    .textCard {
+                        .handles {
+                            height: 12rem;
+                            padding: 0 1.5rem 1.5rem 1.5rem;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @media only screen and (max-width: 960px) {
@@ -252,10 +319,47 @@
     }
 
     @media only screen and (max-width: $breakpoint-tablet) {
+        #heroIllus {
+            .container {
+                .main {
+                    .textCard {
+                        span {
+                            font-size: min(7vw, calc(var(--max-width) * 0.07));
+                        }
+
+                        .handles {
+                            height: 8rem;
+                            padding: 0 1rem 1rem 1rem;
+                        }
+                    }
+                }
+            }
+        }
+
         #search {
             .cities {
                 /* remove top padding */
                 padding-top: 0;
+            }
+        }
+    }
+
+    @media only screen and (max-width: $breakpoint-mobile) {
+        #heroIllus {
+            .container {
+                .main {
+                    .textCard {
+                        span {
+                            padding: 0 var(--pad-md);
+                            font-size: min(10vw, calc(var(--max-width) * 0.10));
+                        }
+
+                        .handles {
+                            height: 8rem;
+                            padding: 0 0.75rem 0.75rem 0.75rem;
+                        }
+                    }
+                }
             }
         }
     }
