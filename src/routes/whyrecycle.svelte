5a81b9be1cb0d19659362f0066615b5df436c01c
Richard Fu <richardfxr.personal@gmail.com>
2022-05-20 23:56:16 -0400

    🟠SVLT: added .card to whyrecycle page

--- src/routes/whyrecycle.svelte
+++ src/routes/whyrecycle.svelte
@@ -4,6 +4,7 @@
     import { onPageLoad } from '$lib/page.js';
     import { curPage } from '../store/store.js';
     import SvgIcon from '$lib/svgIcon.svelte';
+    import Illustration from '$lib/illustration.svelte';
 
     // bindings
     let pageHeading;
@@ -15,76 +16,40 @@
 </script>
 
 
-<div class="twoCol normalWidth" id="intro">
+<div class="twoCol normalWidth" id="intro" aria-labelledby="pageHeading">
     <div class="hero">
 
         <div class="header">
-            <a href="/">
-                <SvgIcon icon="logotype"/>
-            </a>
+            <SvgIcon icon="logotype"/>
         </div>
 
         <h1 id="pageHeading" bind:this={pageHeading} tabindex="-1">Why Recycle?</h1>
+        <p>Recycling magically disappears.<br> We throw it all in a bin, it gets carted away and never see it again.</p>
+    </div>
+
+    <div class="content contentFlex">
+        <section class="card card--vrt">
+            <Illustration illus="blackhole" clr1="neonBlue" clr2="greyCold" type="card--vrt"/>
+            <p>Most people do not know where recycling goes. Even those in the waste industry do not know exactly where our waste ends up.</p>
+        </section>
+        <section class="card card--vrt">
+            <Illustration illus="middleEast-questionMark" type="card--vrt"/>
+            <p>In RI, for example, the main recycling facility, lists  “the Middle East” as a shipping destination. When we inquired further for specific locations, the answer was  “We don’t know because selling is handled by a third-party. Where it goes next changes everyday.”</p>
+        </section>
+        <section class="card card--vrt">
+            <p>Where trash goes in the next process is not treated as our problem– it is always someone else’s. That’s why we decided to dive deep into this seemingly simple question; Where does our recycling really go?</p>
+        </section>
+        <section class="card card--vrt">
+            <Illustration illus="blackhole" clr1="neonBlue" clr2="greyCold" type="card--vrt"/>
+            <p>Most people do not know where recycling goes. Even those in the waste industry do not know exactly where our waste ends up. </p>
+        </section>
     </div>
 </div>
 
-<div class="placeholder"></div>
 
 <style lang="scss">
-    .cityOpt {
-        display: grid;
-        grid-template: 
-            "guidelines facility" 23rem
-            "share facility" 5rem
-            / 1fr 1fr;
-
-        .guidelines {
-            grid-area: guidelines;
-        }
-
-        .share {
-            grid-area: share;
-
-            :global(div:last-child) {
-                :global(.button) {
-                    /* hide mobile share button */
-                    display: none; 
-                }
-            }
-        }
-
-        .facility {
-            grid-area: facility;
-            /* align facility to the bottom */
-            display: flex;
-            justify-content: flex-end;
-        }
-    }
-
 
     /* === BREAKPOINTS ======================== */
     @media only screen and (max-width: $breakpoint-mobile) {
-        .cityOpt {
-            grid-template: 
-                "guidelines facility" 50vw
-                "share facility" 13vw
-                / 1fr 1fr;
-            
-            .share {
-                :global(div:last-child) {
-                    :global(.button) {
-                        /* hide desktop share this page button */
-                        display: flex; 
-                    }
-                }
-
-                :global(div:first-child) {
-                    :global(.button) {
-                        /* show mobile share button */
-                        display: none; 
-                    }
-                }
-            }
-        }
     }
 </style>
